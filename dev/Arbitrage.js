ecd910bf53bfeb7f70c9b4bc9fe4922e83833948
Tabor <89608421+taborgreat@users.noreply.github.com>
2024-01-08 21:59:25 -0800

    initial commit

---
+++ dev/Arbitrage.js
@@ -0,0 +1,82 @@
+/*
+    
+
+*/
+
+//import the exchange objects for use in constructor as one array
+
+import exchanges from './Exchanges/exchangeContainer'
+
+
+class Arbitrage {
+    constructor(exchanges, token, profitMarginGoal){
+        this.exchanges = exchanges;
+        this.token = TOKEN NAME //MUST FOLLOW GLOBAL NAMES
+
+        this.currentBalance = {token: [], stable: []}
+        this.previousBalances = {token:[], stable: []};
+
+        this.currExchange = //initial find lowest price in exchanges exchanges[index]
+        this.targetExchange = //exchanges[index]
+
+        this.profitMarginGoal = profitMarginGoal
+
+    }
+
+    scanExchanges(exchanges){
+
+        //scan the array of exchange objects and replace with price data
+        const exchangePrices = array function to replace exchanges with exchange.getPrice(token)
+
+        return exchangePrices;
+    }
+
+    getLowestExchangePrice(exchangePrices){
+        const lowestExchangePrice = find lowest price in exchangePrices array
+
+        return { price: lowestExchangePrice, targetExchange: index in this.exchanges }
+    }
+
+    getHighestExchangePrice(exchangePrices){
+        const highestExchangePrice = find highest price in exchangePrices array
+
+        return { price: highestExchangePrice, targetExchange: index in this.exchanges }
+    }
+
+    testProfitMargin(currExchange, targetExchange){
+        
+        const margin = (currExchange.getPrice(token) - currExchange.feeAverage)  math
+                        (targetExchange.getPrice(token) - targetExchange.feeAverage)
+
+        return margin;
+    }
+
+    sendToExchange(targetExchange){
+        let sendAttempt = currExchange.send(targetExchange.address)
+        if sendAttempt.success == true
+            this.currExchange = targetExchange
+
+        return sendAttempt;
+    }
+
+    buyToken(){
+        let buyAttempt = this.currExchange.buyToken(token, this.currentBalance.stable // minus fees);
+        if(buyAttempt.success){
+            this.previousBalances.token.push(this.currentBalance.token)
+            this.currentBalance.token = buyAttempt.transactionData.//total amount bought
+
+        }
+        return buyAttempt;
+    }
+
+    swapToStable(){
+        let sellAttempt = this.currExchange.sellToken(token);
+        if(sellAttempt.success){
+            this.previousBalances.stable.push(this.currentBalance.stable)
+            this.currentBalance.stable = buyAttempt.transactionData.//total amount sold
+
+        }
+        return sellAttempt;
+    }
+
+}
---
+++ dev/Exchanges/blueprint.js
@@ -0,0 +1,82 @@
+/* interface
+
+  getWalletBalance(coin or stable address) returns int balance
+  buy(tokenAmount) return {success: bool, transactionData: data}
+  sell(tokenAmount) return {success: bool, transactionData: data}
+  getPrice(token) returns int price
+  send(address) --> returns {success: bool, transactionData: data}
+
+  const feesAmount = total cost of average fees for exchange
+  const token = name of token from GLOBAL list
+
+    may reform as object
+    {name: , address: , balance: , except name is initially required for API}
+  const walletAddressToken = address depending on token
+  const walletAddressStable = address of stable wallet
+  const tokenPrice = price of token
+  const currentTokens = total amount of tokens in wallet
+  const currentStable = total amount of stable coin in wallet
+
+
+*/
+
+
+
+//maybe make a map to map token name from GLOBAL to address per respective object
+
+
+
+class nameOfExchange {
+    constructor(){
+        this.feesAmount = //specifically calculate for each exchange with varying methods
+        this.token = //name of Token from GLOBAL list
+        this.walletAddressToken = //address of token. can be static or defined 
+        this.walletAddressStable = //address of stable coin on exchange. static or defined
+        this.tokenPrice = getPrice(this.token);
+        this.currentTokenAmount = getWalletBalance(this.walletAddressToken);
+        this.currentStableAmount = getWalletBalance(this.walletAddressStable);
+
+    }
+    getWalletBalance(address){
+        //calculate the balance as int
+        const balance =
+
+        return balance(may need to figure out returning eth vs USD values);
+    }
+
+    buyToken(walletAddressToken, amountInStable){
+        //buy the token with proper means, and then fill buyData based off transaction
+    
+
+        const buyData = {success: , transactionData: }
+        return buyData;
+    }
+
+    sellToken(walletAddressToken){
+        //sell the token with proper means, and then fill buyData based off transaction
+        //use calculateFees() to sell max amount
+        
+        const sellData = {success: , transactionData: }
+        return sellData;
+    }
+
+    getPrice(walletAddress){
+        //collect price data
+        const currentPrice =
+
+        return currentPrice
+    }
+
+    send(address){
+        //send to the address the calculateFees value
+
+
+
+        const sendData = {success: , transactionData: }
+        return sendData;
+    }
+
+    calculateFees(){
+        return this.currentTokenAmount - this.feesAmount;
+    }
+}
